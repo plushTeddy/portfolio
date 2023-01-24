e02e75ffc8c2caec0766926fb51d773af0f57212
plushTeddy <janik.schuenemann@bluewin.ch>
2023-01-24 20:27:02 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import router from './router'
 import {MotionPlugin} from "@vueuse/motion";
 import { Icon } from '@iconify/vue';
 
+// noinspection JSUnusedLocalSymbols
 const app = createApp(App)
     .use(router)
     .use(MotionPlugin)
     .component('Icon', Icon)
     .mount('#app')
+
